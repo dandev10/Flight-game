0faecfa2335e0734662d1246488ebef5d18e77de
THEspager <danilagon@icloud.com>
2021-11-07 13:44:25 +0100

    Update

--- flight_game.py
+++ flight_game.py
@@ -1,3 +1,17 @@
-print("hello welcome to flight   (version 0.0.2a)")
+print("hello welcome to flight   (version 0.0.2-2a)")
 name = input("what your name?\n")
 print("hello", name)
+run = True
+while run:
+    print("Write commands below (Write as they are wroten below): ")
+    print("                                                       Fly")
+    print("                                                       About (It will say about the game) ")
+    commands = input(" ")
+    if "Fly" in commands:
+        print("Choose a flight: ")
+        print("                 SAS-804; LONDON-OSLO. Write Sas")
+        print("                 NORWIGIAN-735; HAU-OSLO. Write Nor")
+        print("                 WIZZAIR-356; NYC-LONDON. Write Wizz")
+        flight = input("What flight? ")
+    elif "About" in commands:
+        print("")
