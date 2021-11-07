1a5f1641ef16f52ad1d8ead1334a94127fae48e7
nikeedev <myftr12394@gmail.com>
2021-11-07 18:52:10 +0100

    Updates

--- flight_game.py
+++ flight_game.py
@@ -8,7 +8,11 @@ run = True
 position = "LONDON"
 locations = ["NYC", "TRONDHEIM", "OSLO", "LONDON"] 
 def shuffran():
-    random.
+    if (random.choice(locations) == position):
+        return shuffran
+    else:
+        return random.choice(locations)
+
 
 while run:
     print(f"You are now at {position}")
@@ -18,18 +22,21 @@ while run:
     print("                                                       write Money to(see your flight money)")
     commands = input(" ")
     if "Fly" in commands:
+        flight1 = shuffran()
+        flight2 = shuffran()
+        flight3 = shuffran()
         print("Choose a flight: ")
-        print(f"                 SAS-804; {position}-{random.choice(locations)}. Write Sas")
-        print(f"                 NORWIGIAN-735; {position}-TRONDHEIM. Write Nor")
-        print(f"                 WIZZAIR-356; {position}-NYC. Write Wizz")
+        print(f"                 SAS-804; {position}-{flight1}. Write Sas")
+        print(f"                 NORWIGIAN-735; {position}-{flight2}. Write Nor")
+        print(f"                 WIZZAIR-356; {position}-{flight3}. Write Wizz")
         flight = input("What flight? ")
         if "Sas" in flight:
             print("                write Start to start fly")
             commandsas = input(" ") 
             if "Start" in commandsas:
-                print("flying...") 
+                print("Flying...") 
                 time.sleep(25)
-                position = "OSLO"
+                position = flight1
                 print(f"Welcome to {position}")
         if "Nor" in flight:
             print("                write Start to start fly")
@@ -37,14 +44,16 @@ while run:
             if "Start" in commandnor:
                 print("Flying...")
                 time.sleep(24)
-                print("welcome to Trondheim") 
+                position = flight2
+                print(f"Welcome to {position}") 
         if "Wizz" in flight:
             print("              write Start to start fly")
             commandwiz = input(" ")
             if "Start" in commandwiz:
-                print("flying...")
+                print("Flying...")
                 time.sleep(25)
-                print("welcome to New york")
+                position = flight3
+                print(f"Welcome to {position}")
 
     elif "Money" in commands:
         print("you have now 0 flight coins") 
@@ -55,3 +64,6 @@ while run:
         print("game made by THESpager and nikeedev. this is orginal version of flight game. this game is about")
         print("flight andveturs an full of sky flying and make money and of this money you can unlock new ")
         print("places, airplanes, some of them are faster and make more money. we can`t wait to you try it.")
+
+    else:
+        continue
