b52e2e6ea4a917f9ea0bc62ca03d20796f4b6756
THEspager <danilagon@icloud.com>
2021-11-08 17:01:51 +0100

    Updates

--- game.py
+++ game.py
@@ -3,11 +3,26 @@ import pygame
 pygame.init()
 
 
-screen = pygame.display.set_mode((600, 800))
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+
+
+
 
 pygame.display.set_caption("Flight Game, v.0.0.1")
 
-backgroundcolor = (93, 115, 240)
+
+sea = pygame.image.load('data/sea.png')
+pygame.display.set_icon(sea)
+
+cloud = pygame.image.load('data/cloud.png')
+
+plane = pygame.image.load('data/plane.png')
+
+
+
+backgroundcolor = (48, 131, 255)
 running = True
 while running:
     for event in pygame.event.get():
@@ -19,6 +34,10 @@ while running:
 
     screen.fill(backgroundcolor)
 
+    screen.blit(sea, (1, 346))
+    screen.blit(sea, (462, 346))
+    screen.blit(cloud, (400, 100))
+    screen.blit(cloud, (700, 100))
     
     pygame.display.update()
 
