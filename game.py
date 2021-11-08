f97d8c62ebf50f507b3ce8333835d811009b9da2
THEspager <danilagon@icloud.com>
2021-11-08 15:29:03 +0100

    Updates

--- game.py
+++ game.py
@@ -1,152 +1,27 @@
 import pygame
 
-
 pygame.init()
 
-width = 820
-height = 520
 
+screen = pygame.display.set_mode((600, 800))
 
-img = pygame.image.load('data/person.png')
-pygame.display.set_icon(img)
+pygame.display.set_caption("Flight Game, v.0.0.1")
 
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Astro")
-font = pygame.font.SysFont("arial",  20, bold = pygame.font.Font.bold)
-
-black = (0, 0, 0)
-white = (255, 255, 255)
 backgroundcolor = (93, 115, 240)
-brick = pygame.image.load("data/brick.png")
-rect = brick.get_rect()
-rect.center = (32, 22)
-
-
-
-
-player = pygame.image.load("data/person.png")
-player2 = pygame.image.load("data/person.png")
-playerX = 400
-playerY = 270
-player2X = 440
-player2Y = 290
-move_right = False
-move_left = False
-move_down = False
-move_up = False
-move_right2 = False
-move_left2 = False
-move_down2 = False
-move_up2 = False
 running = True
-
-
-whole_text = ("Player 1: X: ", playerX, "Y: ", playerY, "Player 2: X: ", player2X, "Y: ", player2Y)
-text = font.render(str(whole_text), True, white, backgroundcolor)
-
-
-
-
-def person():
-    screen.blit(player, (playerX, playerY))
-def person2():
-    screen.blit(player2, (player2X, player2Y))
-def bricked():
-    screen.blit(brick, rect)
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                move_left = True
-            if event.key == pygame.K_RIGHT:
-                move_right = True
-            if event.key == pygame.K_DOWN:
-                move_down = True
-            if event.key == pygame.K_UP:
-                move_up = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-               move_left = False
-            if event.key == pygame.K_RIGHT:
-                move_right = False
-            if event.key == pygame.K_DOWN:
-                move_down = False
-            if event.key == pygame.K_UP:
-                move_up = False
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                move_left2 = True
-            if event.key == pygame.K_d:
-                move_right2 = True
-            if event.key == pygame.K_s:
-                move_down2 = True
-            if event.key == pygame.K_w:
-                move_up2 = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                move_left2 = False
-            if event.key == pygame.K_d:
-                move_right2 = False
-            if event.key == pygame.K_s:
-                move_down2 = False
-            if event.key == pygame.K_w:
-                move_up2 = False
-
-    
-    if move_right:
-        playerX += 0.5
-    if move_left:
-        playerX -= 0.5
-    if move_up:
-        playerY -= 0.5
-    if move_down:
-        playerY += 0.5
-
-    if move_right2:
-        player2X += 0.5
-    if move_left2:
-        player2X -= 0.5
-    if move_up2:
-        player2Y -= 0.5
-    if move_down2:
-        player2Y += 0.5
-
-    if playerY >= 500:
-        playerY = 490
-    if playerY <= 0:
-        playerY = 10 
-    if playerX >= 805:
-        playerX = 795
-    if playerX <= 0:
-        playerX = 10 
-
-    if player2Y >= 500:
-        player2Y = 490
-    if player2Y <= 0:
-        player2Y = 10 
-    if player2X >= 805:
-        player2X = 795
-    if player2X <= 0:
-        player2X = 10
 
 
 
 
     screen.fill(backgroundcolor)
 
-    bricked()
-    person()
-    person2()
-    whole_text = ("Player 1: X: ", playerX, "Y: ", playerY, "Player 2: X: ", player2X, "Y: ", player2Y)
-    text = font.render(str(whole_text), True, white, backgroundcolor)
-    screen.blit(text, ((50), height - 50))
-    
-    
-    
-
     
     pygame.display.update()
+
+
+
+    
